7a88f159e2e525a35565b622f835ae7aba796724
Mariely Carvajal <mariely@MacBook-Pro-de-Mariely.local>
2022-07-07 02:59:51 -0500

    Initial

---
+++ config/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const ROOT_DIR = path.join(__dirname, '..');
+const SRC_DIR = path.join(ROOT_DIR, 'src');
+const BUILD_DIR = path.join(ROOT_DIR, 'build');
+
+const mainFile = path.join(SRC_DIR, 'index.tsx');
+
+module.exports = {
+	entry: mainFile,
+	output: {
+		path: BUILD_DIR,
+		filename: 'main.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+					}
+				}
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.tsx']
+	}
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('application'));
+
+root.render(<h1>React User Login</h1>);
+
